146a8676170e75419668a96fb65f523e074ad394
enricop89 <enrico.portolan@gmail.com>
2021-05-25 13:53:23 +0100

    - first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
+/* import { Room } from './library/multiparty-sdk.umd'; */
+
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import VideoRoom from './components/VideoRoom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Switch>
+          {/* <Route path="/about">
+            <About />
+          </Route> */}
+          <Route path="/video-room">
+            <VideoRoom />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/VideoRoom/index.js
@@ -0,0 +1,3 @@
+export default function VideoRoom() {
+  return <div>Video Room</div>;
+}
