d7f2b769ce7a4255402efdac08b67261ff63b5eb
javiermolsanz <javier.molinasanz@vonage.com>
2021-05-26 10:25:22 +0100

    removing client folder

--- src/components/VideoRoom/index.js
+++ src/components/VideoRoom/index.js
@@ -1,7 +1,10 @@
 import { useParams } from 'react-router';
+import { getCredentials } from '../../api/fetchCreds';
 
 export default function VideoRoom() {
   let { roomName } = useParams();
+  // useEffect(() => {}, []);
+
   console.log(roomName);
   return <div>Video Room</div>;
 }
