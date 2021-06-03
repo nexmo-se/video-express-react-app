3406b3efaf029387054e1dda15ed017291404f3a
javiermolsanz <javier.molinasanz@vonage.com>
2021-06-03 15:22:51 +0100

    adding tooltips for toolbar

--- src/components/MuteVideoButton/index.js
+++ src/components/MuteVideoButton/index.js
@@ -2,16 +2,19 @@ import { useCallback } from 'react';
 import VideoCam from '@material-ui/icons/Videocam';
 import VideocamOff from '@material-ui/icons/VideocamOff';
 import { IconButton } from '@material-ui/core';
+import Tooltip from '@material-ui/core/Tooltip';
 
 export default function MuteVideoButton({ classes, hasVideo, toggleVideo }) {
   return (
-    <IconButton
-      edge="start"
-      aria-label="videoCamera"
-      onClick={toggleVideo}
-      className={classes.toolbarButtons}
-    >
-      {hasVideo ? <VideocamOff /> : <VideoCam />}
-    </IconButton>
+    <Tooltip title="Toggle video" aria-label="add">
+      <IconButton
+        edge="start"
+        aria-label="videoCamera"
+        onClick={toggleVideo}
+        className={classes.toolbarButtons}
+      >
+        {hasVideo ? <VideocamOff /> : <VideoCam />}
+      </IconButton>
+    </Tooltip>
   );
 }
--- src/components/RecordingButton/index.js
+++ src/components/RecordingButton/index.js
@@ -5,12 +5,13 @@ import VoiceChatIcon from '@material-ui/icons/VoiceChat';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
 import { IconButton } from '@material-ui/core';
+import Tooltip from '@material-ui/core/Tooltip';
 
 export default function RecordingButton({ classes, room }) {
   const [isRecording, setRecording] = useState(false);
   const [archiveId, setArchiveId] = useState(null);
 
-  const handleRecordingStart = async (sessionId) => {
+  const handleRecordingStart = async sessionId => {
     console.log('starting to record');
     try {
       const data = await startRecording(sessionId);
@@ -26,7 +27,7 @@ export default function RecordingButton({ classes, room }) {
     }
   };
 
-  const handleRecordingStop = async (archiveId) => {
+  const handleRecordingStop = async archiveId => {
     console.log('stopping the recording');
     console.log(archiveId);
     if (isRecording) {
@@ -53,18 +54,20 @@ export default function RecordingButton({ classes, room }) {
   };
 
   return (
-    <IconButton
-      edge="start"
-      color="inherit"
-      aria-label="mic"
-      onClick={handleRecordingAction}
-      className={classes.toolbarButtons}
-    >
-      {isRecording ? (
-        <RadioButtonCheckedIcon fontSize="inherit" />
-      ) : (
-        <VoiceChatIcon fontSize="inherit" />
-      )}
-    </IconButton>
+    <Tooltip title="Recording" aria-label="add">
+      <IconButton
+        edge="start"
+        color="inherit"
+        aria-label="mic"
+        onClick={handleRecordingAction}
+        className={classes.toolbarButtons}
+      >
+        {isRecording ? (
+          <RadioButtonCheckedIcon fontSize="inherit" />
+        ) : (
+          <VoiceChatIcon fontSize="inherit" />
+        )}
+      </IconButton>
+    </Tooltip>
   );
 }
