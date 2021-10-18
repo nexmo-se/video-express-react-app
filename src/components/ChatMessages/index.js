5552926cae797d501c735bea08c979d0754fbab7
javiermolsanz <javier.molinasanz@vonage.com>
2021-10-18 15:55:14 +0100

    fixing chat width

--- src/components/ChatMessages/index.js
+++ src/components/ChatMessages/index.js
@@ -18,40 +18,42 @@ const ChatMessages = ({ chatMessages, chatClass }) => {
   const classes = styles();
   return (
     <div className={chatClass}>
-      {chatMessages && chatMessages.length > 0
-        ? chatMessages.map((msg, key) => {
-            return (
-              <div
-                ref={messagesEndRef}
-                className={`${classes.messageContainer} ${
-                  msg?.from?.name ? '' : classes.myMessage
-                }`}
-                key={key}
-              >
-                <div className={classes.chatAvatar}>
-                  <ContactsIcon className={classes.iconChat} />
-                  <Typography color="textSecondary" variant="subtitle1">
-                    {msg?.from?.name ? `${msg.from.name}:` : 'Me:'}
-                    {/* <span className={classes.time}>{msg.date}</span> */}
-                  </Typography>
-                  <Typography
-                    className={classes.time}
-                    color="textSecondary"
-                    variant="subtitle1"
-                  >
-                    {msg.date}
-                  </Typography>
-                </div>
-                <div className={classes.chatAvatar}>
-                  {/* <ChatIcon className={classes.iconChat} /> */}
-                  <Typography color="textPrimary" variant="body1">
-                    {msg.data}
-                  </Typography>
-                </div>
+      {chatMessages && chatMessages.length > 0 ? (
+        chatMessages.map((msg, key) => {
+          return (
+            <div
+              ref={messagesEndRef}
+              className={`${classes.messageContainer} ${
+                msg?.from?.name ? '' : classes.myMessage
+              }`}
+              key={key}
+            >
+              <div className={classes.chatAvatar}>
+                <ContactsIcon className={classes.iconChat} />
+                <Typography color="textSecondary" variant="subtitle1">
+                  {msg?.from?.name ? `${msg.from.name}:` : 'Me:'}
+                  {/* <span className={classes.time}>{msg.date}</span> */}
+                </Typography>
+                <Typography
+                  className={classes.time}
+                  color="textSecondary"
+                  variant="subtitle1"
+                >
+                  {msg.date}
+                </Typography>
               </div>
-            );
-          })
-        : <div>There are no messages</div>}
+              <div className={classes.chatContent}>
+                {/* <ChatIcon className={classes.iconChat} /> */}
+                <Typography color="textPrimary" variant="body1">
+                  {msg.data}
+                </Typography>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <div>There are no messages</div>
+      )}
     </div>
   );
 };
