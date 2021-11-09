59eb75da56602cebcd263a257f050ca7c68056a0
javiermolsanz <javier.molinasanz@vonage.com>
2021-11-09 09:25:17 +0000

    removing console log

--- src/api/fetchCreds.js
+++ src/api/fetchCreds.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 let API_URL = `${process.env.REACT_APP_API_URL_DEVELOPMENT}`;
-console.log(process.env, API_URL);
 if (process.env.NODE_ENV === 'production') {
   API_URL = `${process.env.REACT_APP_API_URL_PRODUCTION}`;
 }
